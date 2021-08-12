240bd82b445e2d6b91f39a2b8899b22fe29c7b54
cryptoman-gg <79981527+cryptoman-gg@users.noreply.github.com>
2021-08-12 16:51:59 +0900

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,14 +12,12 @@ const About = () => {
         <img src={papan01} className="user-avatar" alt="papan01" />
         <section className="about-context">
           <div>
-            <h3>關於我</h3>
-            <p className="time">2019-12-11</p>
+            <h3>タクちゃん</h3>
+            <p className="time">2021-08-12</p>
             <p>
-              大學念數學，碩士唸資工，自學程式，目前主要學習的方向為JavaScript與ReactJS，因為工作的關係
-              ，也有架設過整個後端資料庫與編寫API的經驗。 一直以來都想自己做個blog來記錄與分享自己的學習路程，
-              而這個blog也成為我第一個獨立完成的網頁，這個blog使用GatsbyJS所寫，若有興趣可以看看
-              <a href="https://github.com/papan01/gatsby-starter-papan01">gatsby-starter-papan01</a>
-              ，若有任何問題歡迎使用下方連結與我聯繫。
+              東京大学を卒業し、仮想通貨投資をはじめ1年で億り人を達成。<a href="https://h.accesstrade.net/sp/cc?rk=0100nerr00lj0h">コインチェック</a>
+　　　　　　　　で投資を開始し、1か月後に<a href="https://www.binance.com/ja/register?ref=WSWDIJ94">バイナンス</a>へ移行し原資を蓄える。バイナンスからすぐに魔界デビュー
+　　　　　　　　moonrat,GMR,NFTartなどの100倍銘柄をうまくつかみ億り人を達成
             </p>
           </div>
         </section>
